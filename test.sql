5c70f4a7f5a87a4fc90980e24970b150eaf973c9
Kiki <Dornerova>
2025-03-19 21:17:29 +0100

    Select II.

--- test.sql
+++ test.sql
@@ -4,3 +4,10 @@ WHERE code NOT IN (
     SELECT district_code
     FROM not_completed_provider_info_district
 );
+
+SELECT *
+FROM czechia_distric
+WHERE code NOT IN (
+    SELECT district_code
+    FROM not_completed_provider_info_district
+);
